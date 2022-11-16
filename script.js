841c8b8ab806c3bde268138b278328ab91cca2dd
Taryn King <marshaeking@gmail.com>
2022-11-16 17:05:23 -0600

    complete core DOM exercises

---
+++ script.js
@@ -0,0 +1,46 @@
+const message = document.querySelector('#message');
+
+
+
+const addMovie = (event) => {
+
+    event.preventDefault();
+
+    let inputField = document.querySelector('input');
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement('span');
+
+    movieTitle.innerHTML = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie);
+
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    document.querySelector('ul').appendChild(movie);
+
+    inputField.value = '';
+}
+
+document.querySelector('form').addEventListener('submit', addMovie);
+
+
+
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+    message.textContent = 'Movie Deleted!';
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked');
+
+    if (event.target.classList.contains('checked')) {
+        message.textContent = 'Movie watched!';
+    } else {
+        message.textContent = 'Movie added back!';
+    }
+}
