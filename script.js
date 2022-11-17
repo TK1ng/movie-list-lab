e8cb5c5dac29849a19577b0db791de03bdd0b02b
Taryn King <marshaeking@gmail.com>
2022-11-16 18:41:22 -0600

    add notification to log movie titles

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ const addMovie = (event) => {
 
     document.querySelector('ul').appendChild(movie);
 
+    message.textContent = `${inputField.value} added`;
+
     inputField.value = '';
+    revealMessage();
 }
 
 document.querySelector('form').addEventListener('submit', addMovie);
@@ -32,15 +35,25 @@ document.querySelector('form').addEventListener('submit', addMovie);
 
 function deleteMovie(event) {
     event.target.parentNode.remove();
-    message.textContent = 'Movie Deleted!';
+    message.textContent = `${event.target.textContent} deleted`;
+    revealMessage();
+
 }
 
 function crossOffMovie(event) {
     event.target.classList.toggle('checked');
 
     if (event.target.classList.contains('checked')) {
-        message.textContent = 'Movie watched!';
+        message.textContent = `${event.target.textContent} watched!`;
     } else {
-        message.textContent = 'Movie added back!';
+        message.textContent = `${event.target.textContent} added back!`;
     }
+    revealMessage();
+}
+
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide');
+    }, 1000)
 }
